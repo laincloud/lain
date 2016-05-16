272fcc4e6144abf161e23349c686c226608cd785
chenyunfei <yunfeichen@creditease.cn>
2016-05-16 10:44:00 +0800

    add prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+filename="lain-pre-1462250588.tar.gz"
+if [ ! -f "$filename" ]; then
+    wget http://lain.oss-cn-beijing.aliyuncs.com/${filename}
+fi
+mkdir playbooks/roles/binary/files/
+tar -xzvf $filename -C playbooks/roles/binary/files/
