f6227d96f9e2ba65309fcfe8516245cb0c8c726e
Zhuoyun Wei <zhuoyunwei@creditease.cn>
2016-05-18 14:18:35 +0800

    Use https:// download URL

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 
 filename="lain-pre-1462250588.tar.gz"
 if [ ! -f "$filename" ]; then
-    wget http://lain.oss-cn-beijing.aliyuncs.com/${filename}
+    wget https://lain.oss-cn-beijing.aliyuncs.com/${filename}
 fi
 mkdir playbooks/roles/binary/files/
 tar -xzvf $filename -C playbooks/roles/binary/files/
