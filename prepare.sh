5098530cac2f605ee69bf34a452e681bf3d0d8c6
wenbinmeng <wenbinmeng@creditease.cn>
2016-10-20 14:36:11 +0800

    Updated docker to version 1.12.1

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-filename="lain-pre-1466238079.tar.gz"
+filename="lain-pre-1476939656.tar.gz"
 if [ ! -f "$filename" ]; then
     wget https://lain.oss-cn-beijing.aliyuncs.com/${filename}
 fi
