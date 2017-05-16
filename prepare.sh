935aad35fbe4d6e24b96f7ca0ef8e80b633e38ce
Yongzhi Pan <yongzhipan@creditease.cn>
2017-05-16 10:54:59 +0800

    Fix mkdir.

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,6 @@ filename="lain-pre-1476939656.tar.gz"
 if [ ! -f "$filename" ]; then
     wget https://lain.oss-cn-beijing.aliyuncs.com/${filename}
 fi
-mkdir playbooks/roles/binary/files/
+pre_dir=playbooks/roles/binary/files/
+[ -d $pre_dir ] || mkdir $pre_dir
 tar -xzvf $filename -C playbooks/roles/binary/files/
