eadeb7995e089366e9f342ddebebd93f16e9427d
lichao-mobanche <494232356@qq.com>
2017-10-18 15:38:37 +0800

    添加延时测试，取消断点续传

--- prepare.sh
+++ prepare.sh
@@ -14,6 +14,7 @@ LIBNETWORK_PLUGIN_VERSION="v1.1.0"
 BIRD_VERSION="v0.3.1"
 BIRD6_VERSION="v0.3.1"
 CONSUL_VERSION="0.8.5"
+ETCD_VERSION="2.3.7"
 BINARY_URL_PREFIX="https://lain.oss-cn-beijing.aliyuncs.com/binary"
 
 filename="lain-pre-1476939656.tar.gz"
@@ -30,18 +31,20 @@ mkdir -p playbooks/roles/lainlet/files
 mkdir -p playbooks/roles/calico/files/bin
 mkdir -p playbooks/roles/consul/files/bin
 
-wget -c ${BINARY_URL_PREFIX}/networkd/releases/download/${NETWORKD_VERSION}/networkd.xz -O playbooks/roles/networkd/files/networkd.xz
-wget -c ${BINARY_URL_PREFIX}/deployd/releases/download/${DEPLOYD_VERSION}/deployd.xz -O playbooks/roles/deployd/files/deployd.xz
-wget -c ${BINARY_URL_PREFIX}/lainlet/releases/download/${LAINLET_VERSION}/lainlet.xz -O playbooks/roles/lainlet/files/lainlet.xz
-wget -c ${BINARY_URL_PREFIX}/calicoctl/releases/download/${CALICOCTL_VERSION}/calicoctl.xz -O playbooks/roles/calico/files/bin/calicoctl.xz
-wget -c ${BINARY_URL_PREFIX}/calicoctl/releases/download/${ALLOCATE_IPIP_ADDR_VERSION}/allocate-ipip-addr.xz -O playbooks/roles/calico/files/bin/allocate-ipip-addr.xz
-wget -c ${BINARY_URL_PREFIX}/calicoctl/releases/download/${STARTUP_VERSION}/startup.xz -O playbooks/roles/calico/files/bin/startup.xz
-wget -c ${BINARY_URL_PREFIX}/felix/releases/download/${CALICO_FELIX_VERSION}/calico-felix.xz -O playbooks/roles/calico/files/bin/calico-felix.xz
-wget -c ${BINARY_URL_PREFIX}/confd/releases/download/${CONFD_VERSION}/confd.xz -O playbooks/roles/calico/files/bin/confd.xz
-wget -c ${BINARY_URL_PREFIX}/libnetwork-plugin/releases/download/${LIBNETWORK_PLUGIN_VERSION}/libnetwork-plugin.xz -O playbooks/roles/calico/files/bin/libnetwork-plugin.xz
-wget -c ${BINARY_URL_PREFIX}/bird/releases/download/${BIRD_VERSION}/bird.xz -O playbooks/roles/calico/files/bin/bird.xz
-wget -c ${BINARY_URL_PREFIX}/bird/releases/download/${BIRD6_VERSION}/bird6.xz -O playbooks/roles/calico/files/bin/bird6.xz
-wget -c ${BINARY_URL_PREFIX}/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -O playbooks/roles/consul/files/bin/consul_${CONSUL_VERSION}_linux_amd64.zip
+wget  ${BINARY_URL_PREFIX}/networkd/releases/download/${NETWORKD_VERSION}/networkd.xz -O playbooks/roles/networkd/files/networkd.xz
+wget  ${BINARY_URL_PREFIX}/deployd/releases/download/${DEPLOYD_VERSION}/deployd.xz -O playbooks/roles/deployd/files/deployd.xz
+wget  ${BINARY_URL_PREFIX}/lainlet/releases/download/${LAINLET_VERSION}/lainlet.xz -O playbooks/roles/lainlet/files/lainlet.xz
+wget  ${BINARY_URL_PREFIX}/calicoctl/releases/download/${CALICOCTL_VERSION}/calicoctl.xz -O playbooks/roles/calico/files/bin/calicoctl.xz
+wget  ${BINARY_URL_PREFIX}/calicoctl/releases/download/${ALLOCATE_IPIP_ADDR_VERSION}/allocate-ipip-addr.xz -O playbooks/roles/calico/files/bin/allocate-ipip-addr.xz
+wget  ${BINARY_URL_PREFIX}/calicoctl/releases/download/${STARTUP_VERSION}/startup.xz -O playbooks/roles/calico/files/bin/startup.xz
+wget  ${BINARY_URL_PREFIX}/felix/releases/download/${CALICO_FELIX_VERSION}/calico-felix.xz -O playbooks/roles/calico/files/bin/calico-felix.xz
+wget  ${BINARY_URL_PREFIX}/confd/releases/download/${CONFD_VERSION}/confd.xz -O playbooks/roles/calico/files/bin/confd.xz
+wget  ${BINARY_URL_PREFIX}/libnetwork-plugin/releases/download/${LIBNETWORK_PLUGIN_VERSION}/libnetwork-plugin.xz -O playbooks/roles/calico/files/bin/libnetwork-plugin.xz
+wget  ${BINARY_URL_PREFIX}/bird/releases/download/${BIRD_VERSION}/bird.xz -O playbooks/roles/calico/files/bin/bird.xz
+wget  ${BINARY_URL_PREFIX}/bird/releases/download/${BIRD6_VERSION}/bird6.xz -O playbooks/roles/calico/files/bin/bird6.xz
+wget  ${BINARY_URL_PREFIX}/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -O playbooks/roles/consul/files/bin/consul_${CONSUL_VERSION}_linux_amd64.zip
+wget  http://lain.oss-cn-beijing.aliyuncs.com/etcd-${ETCD_VERSION}-4.el7.x86_64.rpm -O playbooks/roles/etcd/files/etcd-${ETCD_VERSION}-4.el7.x86_64.rpm
+
 
 unxz -kf playbooks/roles/networkd/files/networkd.xz
 unxz -kf playbooks/roles/deployd/files/deployd.xz
