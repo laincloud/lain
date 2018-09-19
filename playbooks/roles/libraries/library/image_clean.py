09c77d8d0ebfeb18703771f5bc8854ed6798256c
mac@lowang <longhuiwang@creditease.cn>
2018-09-19 16:26:46 +0800

    CLOUD-1301: add filter for rebellion images when clean

--- playbooks/roles/libraries/library/image_clean.py
+++ playbooks/roles/libraries/library/image_clean.py
@@ -17,19 +17,23 @@ def main():
     )
     images = module.params['images']
     redundancy = module.params['redundancy']
+    filter_key_list = ["rebellion", "swarm"]
 
     tag_type = ['release', 'meta']
     save_image_set = set()
     try:
         for image in images:
             for tag in tag_type:
-                save_image_set = save_image_set | get_save_images(image, tag, redundancy)
+                save_image_set = save_image_set | get_save_images(image, tag, redundancy, filter_key_list=filter_key_list)
         module.exit_json(changed=remove_extra_images(save_image_set))
     except Exception as e:
         module.fail_json(msg=str(e))
 
 
-def get_save_images(image, tag_type, redundancy):
+def get_save_images(image, tag_type, redundancy, filter_key_list=None):
+    if filter_key_list is None:
+        filter_key_list = []
+
     try:
         docker_images_output = check_output(['docker', 'images', image], stderr=STDOUT)
     except CalledProcessError as e:
@@ -43,8 +47,13 @@ def get_save_images(image, tag_type, redundancy):
     # 0:REPOSITORY, 1:TAG, 2:IMAGE ID, 3:CREATED, 4:VIRTUAL SIZE
     for row in image_list:
         cols = [i.strip() for i in row.split('  ') if i]  # Columns are seperated by at least two spaces
+        image_name = '{}:{}'.format(cols[0], cols[1])
         if cols[1].startswith(tag_type):
-            image_names.append('{}:{}'.format(cols[0], cols[1]))
+            image_names.append(image_name)
+         # keep those images with tag not start with tag_type
+        for key in filter_key_list:
+            if key in image_name:
+                image_names.append(image_name)
 
     image_names = deduplicate(image_names)
 
